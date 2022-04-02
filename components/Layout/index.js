e2bb16fd835fb2028ef6b5a1cd2363c098288171
WhiteHanClean <whitehanclean@gmail.com>
2022-04-02 12:24:48 +0300

    add router

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,10 +1,49 @@
 import Navbar from './Navbar/Navbar';
 import Footer from './Footer/Footer';
+import styles from './Navbar/Navbar.module.scss';
+import Link from 'next/link'
 
 function Layout({ children }) {
   return (
     <div>
       <Navbar />
+      <div className={styles.navbar_routes}>
+        <div>
+          <Link href={'/'}>
+            <a >главная</a> 
+          </Link>
+        </div>
+        <div>
+        <Link href='/'>
+            <a>каталог</a> 
+        </Link>
+        </div>
+        <div>
+        <Link href={'/'}>
+            <a>о магазине</a> 
+        </Link>
+        </div>
+        <div>
+        <Link href={'/'}>
+            <a>Как заказать</a> 
+        </Link>
+        </div>
+        <div>
+        <Link href={'/'}>
+            <a>Доставка</a> 
+        </Link>
+        </div>
+        <div>
+        <Link href={'/'}>
+            <a>Отзывы</a> 
+        </Link>
+        </div>
+        <div>
+        <Link href={'/'}>
+            <a>Контакты</a> 
+        </Link>
+        </div>
+      </div>
       {children}
       <Footer />
     </div>
