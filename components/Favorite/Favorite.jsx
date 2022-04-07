ceb99db91eb82c4d589b5df1a704474f03308ec9
aibek shamshiev <aibek@MacBook-Air-aibek.local>
2022-04-07 22:34:35 +0600

    adaptive favor

--- components/Favorite/Favorite.jsx
+++ components/Favorite/Favorite.jsx
@@ -5,9 +5,11 @@ import ClothesCard from '../MainPageCards/ClothesCard';
 
 function Favorite() {
   return (
-    <div className={styles.favorite_wrapper}>
-      <ClothesCard cardArray={cardArray} />
-      <div>
+    <div className={styles.favorite}>
+      <div className={styles.favorite_wrapper}>
+        <ClothesCard cardArray={cardArray} />
+      </div>
+      <div className={styles.all_catalog}>
         <button className={styles.all_catalog__button}>смотреть каталог</button>
       </div>
     </div>
